f3d24c06abe036f7495608b9a5b8f67bcf9cee87
Marko Sanković <msankovic@synapticon.com>
2019-06-25 14:25:53 +0200

    Add all cases to request command

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-DEBUG=* npm run motion-master-client request --device-position=0 getSystemVersion
-DEBUG=* npm run motion-master-client request --device-position=0 getDeviceInfo
+DEBUG=* npm run motion-master-client request getSystemVersion
+DEBUG=* npm run motion-master-client request getDeviceInfo
 DEBUG=* npm run motion-master-client request --device-position=0 getDeviceParameterInfo
 DEBUG=* npm run motion-master-client request --device-position=0 getDeviceParameterValues 0x607B:1 0x607B:2
 DEBUG=* npm run motion-master-client request --device-position=0 getDeviceFileList
