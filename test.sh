a8953261fed18fbbce015d60e8b3d380b6a5d744
Marko Sanković <msankovic@synapticon.com>
2019-06-25 11:39:17 +0200

    Add test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-DEBUG=* node dist/cli request getSystemVersion
-DEBUG=* node dist/cli request getDeviceInfo
-DEBUG=* node dist/cli --device-position=0 request getDeviceParameterInfo
-DEBUG=* node dist/cli --device-position=0 request getDeviceParameterValues 0x607B:1 0x607B:2
-DEBUG=* node dist/cli --device-position=0 request getDeviceFileList
-DEBUG=* node dist/cli --device-position=0 request getDeviceLog
+DEBUG=* node dist/cli request --device-position=0 getSystemVersion
+DEBUG=* node dist/cli request --device-position=0 getDeviceInfo
+DEBUG=* node dist/cli request --device-position=0 getDeviceParameterInfo
+DEBUG=* node dist/cli request --device-position=0 getDeviceParameterValues 0x607B:1 0x607B:2
+DEBUG=* node dist/cli request --device-position=0 getDeviceFileList
+DEBUG=* node dist/cli request --device-position=0 getDeviceLog
+DEBUG=* node dist/cli upload --device-position=0 0x607B:1 0x607B:2
+DEBUG=* node dist/cli download --device-position=0 0x2705=3
