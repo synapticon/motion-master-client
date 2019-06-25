4cff386507f46a13e8e542e6fd372b5eaa65f14e
Marko Sanković <msankovic@synapticon.com>
2019-06-25 11:42:54 +0200

    Have the same interface as binary in package.json

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-DEBUG=* node dist/cli request --device-position=0 getSystemVersion
-DEBUG=* node dist/cli request --device-position=0 getDeviceInfo
-DEBUG=* node dist/cli request --device-position=0 getDeviceParameterInfo
-DEBUG=* node dist/cli request --device-position=0 getDeviceParameterValues 0x607B:1 0x607B:2
-DEBUG=* node dist/cli request --device-position=0 getDeviceFileList
-DEBUG=* node dist/cli request --device-position=0 getDeviceLog
-DEBUG=* node dist/cli upload --device-position=0 0x607B:1 0x607B:2
-DEBUG=* node dist/cli download --device-position=0 0x2705=3
+DEBUG=* npm run motion-master-client request --device-position=0 getSystemVersion
+DEBUG=* npm run motion-master-client request --device-position=0 getDeviceInfo
+DEBUG=* npm run motion-master-client request --device-position=0 getDeviceParameterInfo
+DEBUG=* npm run motion-master-client request --device-position=0 getDeviceParameterValues 0x607B:1 0x607B:2
+DEBUG=* npm run motion-master-client request --device-position=0 getDeviceFileList
+DEBUG=* npm run motion-master-client request --device-position=0 getDeviceLog
+DEBUG=* npm run motion-master-client upload --device-position=0 0x607B:1 0x607B:2
+DEBUG=* npm run motion-master-client download --device-position=0 0x2705=3
